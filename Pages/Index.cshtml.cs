eb261b34f785b65a838f9a0fb7b238ff9ecdf8a4
Daniela Olarte <danielaolarte123@gmail.com>
2022-02-06 15:17:15 -0500

    programa de registro de red de programacion competitiva

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesUser.Models;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPCAuthenticationSystem.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesUsers.Data.RazorPagesUsersContext _context;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, RazorPagesUsers.Data.RazorPagesUsersContext? context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public new IList<User> User { get; set; }
+        [TempData]
+        public string Message { get; set; }
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+        [Required]
+        [BindProperty]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                Message = null;
+                return Page();
+            }
+            var TotalUsers = from m in _context.User select m;
+            var user = TotalUsers.Where(s => s.Username.Equals(Username));
+            User = await user.ToListAsync();
+            if (User.Count == 1)
+            {
+                if (User.ElementAt(0).Password.Equals(Password))
+                {
+                    return RedirectToPage("Users/Index");
+                }
+                Message = "La contraseña no coincide a este usuario";
+            }
+            else
+            {
+                Message = "No se han encontrado usuarios con este username";
+            }
+            return Page();
+        }
+    }
+}
